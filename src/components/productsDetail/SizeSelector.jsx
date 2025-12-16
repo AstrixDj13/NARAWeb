43e0e5a9db04aadda9c7503008bd9b719f598b13
Debjit Majumder <debjit.ankur@gmail.com>
2025-12-16 15:15:35 +0000

    size chart added in product page

--- src/components/productsDetail/SizeSelector.jsx
+++ src/components/productsDetail/SizeSelector.jsx
@@ -1,36 +1,9 @@
-import { useState } from "react";
-import { HiOutlineArrowRight } from "react-icons/hi";
 export default function SizeSelector({ sizes, defaultSize, selectSize }) {
-  const [sizeGuideVisible, setSizeGuideVisible] = useState(false);
-
-  const toggleSizeGuideVisibility = () => {
-    setSizeGuideVisible((state) => !state);
-  };
 
   return (
     <div className="flex flex-col gap-4 tracking-tighter">
-      {/* Size Guide */}
-      {sizeGuideVisible && (
-        <div
-          onClick={toggleSizeGuideVisibility}
-          className="bg-black/50 fixed  top-0 right-0 bottom-0 left-0 z-[9900000] flex  bg-[rgb()] items-center justify-center"
-        >
-          <img
-            title="image"
-            onClick={(e) => e.stopPropagation()}
-            className="xl:h-full h-5/6"
-            src="/productDetail/sizeGuide.jpeg"
-            alt=""
-          />
-        </div>
-      )}
       <h2 className="font-bold text-lg">
-        Select Size |{" "}
-        <button className="underline" onClick={toggleSizeGuideVisibility}>
-          {" "}
-          Size Guide
-        </button>{" "}
-        <HiOutlineArrowRight className="inline" />
+        Select Size
       </h2>
 
       <div className="flex gap-4">
@@ -54,6 +27,16 @@ export default function SizeSelector({ sizes, defaultSize, selectSize }) {
             </button>
           ))} */}
       </div>
+
+      {/* Size Chart Image */}
+      <div className="mt-4">
+        <img
+          title="Size Chart"
+          src="/size_chart.jpg"
+          alt="Size Chart"
+          className="w-full max-w-2xl"
+        />
+      </div>
     </div>
   );
 }
@@ -67,9 +50,8 @@ function SizeItem({ name, defaultSizeName, selectSize, enabled }) {
     <button
       title={enabled ? name : "Not available with other selected options!"}
       onClick={clickHandler}
-      className={`border-2 w-8 h-8 border-[#BEBCBD] disabled:opacity-25   ${
-        name === defaultSizeName ? "bg-[#1F4A40] text-white" : ""
-      }`}
+      className={`border-2 w-8 h-8 border-[#BEBCBD] disabled:opacity-25   ${name === defaultSizeName ? "bg-[#1F4A40] text-white" : ""
+        }`}
       disabled={!enabled}
     >
       {name}
