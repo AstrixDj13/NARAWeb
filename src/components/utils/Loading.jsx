8dc0ba134260f646a8a75899a5be07c027396545
Debjit Majumder <debjit.ankur@gmail.com>
2026-09-02 19:19:47 +0100

    transition mod

--- src/components/utils/Loading.jsx
+++ src/components/utils/Loading.jsx
@@ -1,4 +1,7 @@
-import classes from "./Loading.module.css"
 export default function Loading() {
-  return <div className={classes.loader}></div>
+  return (
+    <div className="fixed inset-0 z-50 flex items-center justify-center bg-white/60 dark:bg-black/60 backdrop-blur-sm">
+      <img src="/cat.gif" alt="Loading..." className="w-24 h-24 object-contain" />
+    </div>
+  );
 }
